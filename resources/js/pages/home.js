d0fbaa48928ee5322e22b1945ff02c3996dc7033
gyrov <gyrovbogdan@gmail.com>
2024-07-23 13:49:53 +0300

    Refactor js and fix bugs

--- resources/js/pages/home.js
+++ resources/js/pages/home.js
@@ -1,9 +1,17 @@
 import Api from "../utils/api";
-import Tasks from "../utils/task";
+import EventManager from "../utils/eventManager";
+import DisplayManager from "../utils/displayManager";
+import AppController from "../utils/appController";
 
 const token = $("#api-token").data("token");
 const url = "/api/tasks";
 const api = new Api(token, url);
-const tasks = new Tasks(api);
 
-$(tasks.init());
+const eventManager = new EventManager(api);
+
+const $container = $("#tasks");
+const displayManager = new DisplayManager($container);
+
+const appController = new AppController(displayManager, eventManager, api);
+
+$(appController.init());
